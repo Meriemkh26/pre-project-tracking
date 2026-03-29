44ae405e019265391587d79ba5f2ae8053731b77
Meriemkh26 <meriemkh690@gmail.com>
2026-03-30 00:10:19 +0100

    fix: working project setup in htdocs

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,16 +1,18 @@
 <?php
-require_once '../config/database.php';
+    require_once '../config/database.php';
 
-$request = $_SERVER['REQUEST_URI'];
+    $request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $request = str_replace('/pre-project-tracking/backend/public', '', $request);
 
-switch ($request) {
-    case '/':
-    case '/login':
-        require '../views/login.php';
-        break;
-    default:
-        http_response_code(404);
-        echo "404 - Page not found";
-        break;
-}
+    switch ($request) {
+        case '/':
+        case '':
+        case '/login':
+            require '../views/login.php';
+            break;
+        default:
+            http_response_code(404);
+            echo "404 - Page not found";
+            break;
+    }
 ?>
